ab6f0f463b8eed80ab4a72c6c3eb267b2c7d7b8e
Mert Bozkurtlar <mert.bozkurtlar@outlook.com>
2021-04-20 23:31:57 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,6 @@
 const nameLogo = document.getElementById("nameLogo")
-const hero = document.getElementById("hero")
 
 if(innerWidth < 1440 || innerHeight <900){
-    hero.style.top= "10%"
-    hero.style.left= "5%"
     nameLogo.style.display = "none"
 }
 else{
@@ -11,13 +8,9 @@ else{
 }
 addEventListener("resize", ()=>{
     if(innerWidth < 1440 || innerHeight <900){
-        hero.style.top= "10%"
-        hero.style.left= "5%"
         nameLogo.style.display = "none"
     }
     else{
-        hero.style.top= "22%"
-        hero.style.left= "16%"
         nameLogo.style.display = ""
     }
 })
